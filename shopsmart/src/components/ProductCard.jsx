472561dfcd4f998296cdccdf10ebff4c426c1454
Bikash61 <148321577+Bikash61@users.noreply.github.com>
2026-07-03 19:01:06 +1000

    Second update

---
+++ shopsmart/src/components/ProductCard.jsx
@@ -0,0 +1,9 @@
+
+function ProductCard({product}){
+    return <>
+    <h1>{product.name}</h1>
+    <h2>{product.price}</h2>
+    <button>Add to Card </button>
+    </>;
+}
+export default ProductCard; 
--- shopsmart/src/pages/Home.jsx
+++ shopsmart/src/pages/Home.jsx
@@ -1,12 +1,28 @@
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Footer from "../components/Footer";
+import ProductCard from "../components/ProductCard";
+
+
+const products = [
+    {id:1,name:"Nike Shoes",price:120},
+    {id:2,name:"Adidas Shoes",price:97},
+    {id:3,name:"Puma Shoes",price:110}
+]
+
 
 function Home(){
     return (
     <>
     <Navbar/>
     <Hero/>
+
+    {
+        products.map((items)=>(
+        <ProductCard key={items.id} product = {items}/>
+
+        ))
+    }
     <Footer/>
     </>
     );
